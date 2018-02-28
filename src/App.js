975603864ab79c659223a974175f9a19a8793bdc
wj1453 <286521235@qq.com>
2018-02-28 17:11:20 +0800

    新增select组件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import Counter from './components/Counter';
+import Select from './components/Select';
+
+
+var assets=[
+  {value:'BNB',name:'BNB'},
+  {value:'BTC',name:'BTC'},
+  {value:'ETH',name:'ETH'},
+  {value:'NEO',name:'NEO'},
+]  
 
 class App extends Component {
+
   render() {
     return (
      <div className="app">
@@ -10,6 +20,7 @@ class App extends Component {
         <Counter defaultCount={100} step={2}/>
         <Counter defaultCount={1} step={3}/>
         <Counter defaultCount={2} step={4}/>
+        <Select placeholder="请选择" lists={assets}/>
      </div>
     );
   }
--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,19 +1,52 @@
-import React, {Components} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import './index.css'
 
-class Select extends Components {
+class Select extends Component {
     constructor(props){
         super(props);
+        this.state={
+            showList:false,
+            value:''
+        };
+        this.toggleList=this.toggleList.bind(this);
+        this.selectValue=this.selectValue.bind(this);
     };
+
+    toggleList(){
+        this.setState({
+            showList:!this.state.showList
+        })
+    }
+
+    selectValue(selected){
+        this.setState({
+            value:selected.value
+        })
+    }
+
+    
     render(){
+        const listDom=[];
+        this.props.lists.map(function(v,i){
+            return listDom.push(<li key={i} data-value={v.value} onClick={this.selectValue.bind(this,v)}>{v.name}</li>)
+         },this)
         return (
-            <div className="select">
-                <input type="text" readonly/><i className="iconfont icon-downsj"></i>
-                <ul>
-                    <li>1231</li>
+            <div className="select" onClick={this.toggleList}>
+                <input type="text" value={this.state.value} placeholder={this.props.placeholder} readOnly/><i className="iconfont icon-downsj"></i>
+                <ul className={this.state.showList?'f-db':'f-dn'}>
+                    {
+                        listDom
+                    }
                 </ul>
             </div>
         )
     }
+}
+
+Select.propTypes={
+    placeholder:PropTypes.string,
+    lists:PropTypes.array
 }
 
 export default Select;
